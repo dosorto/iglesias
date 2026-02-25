8c231bf3f7935bf872d52582b24a8e2b77fee59b
Ury Aguirre <ury.aguirre@unah.hn>
2026-02-25 00:32:30 -0600

    Merge con la Rama Pinto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,38 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // 2) Roles y permisos (siempre primero)
         $this->call(RolesAndPermissionsSeeder::class);
 
         // 3) Catálogos/base (antes de tablas que dependan de ellos)
         $this->call(TipoCursoSeeder::class);
-        $this->call(IglesiaSeeder::class);
         $this->call(ReligionSeeder::class);
+
+        // 4) Entidades base
+        $this->call(IglesiaSeeder::class);
+        $this->call(PersonaSeeder::class);
+
+        // 5) Relaciones base
+        $this->call(FeligresSeeder::class);
+
+        // 6) Dependientes de feligreses
+        $this->call(EncargadoSeeder::class);
+        $this->call(InstructorSeeder::class);
+
+        // 7) Cursos e inscripciones
+        $this->call(CursoSeeder::class);
+        $this->call(InscripcionCursoSeeder::class);
+
+        // 8) Sacramentos
+        $this->call(BautismoSeeder::class);
+        $this->call(PrimeraComunionSeeder::class);
+        $this->call(ConfirmacionSeeder::class);
+        $this->call(MatrimonioSeeder::class);
+
+        // 9) Emisiones / certificados
+        $this->call(ConstanciaSeeder::class);
+        $this->call(EmisionDetalleSeeder::class);
+        $this->call(CertificadoCursoSeeder::class);
     }
 }
