7fc2e79f2e898cec1c2b1ec68d4c9272efc45683
PERSONAL <angelpinto098@gmail.com>
2026-03-17 22:36:45 -0600

    merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,43 @@ export default {
         "./resources/**/*.vue",
         "./node_modules/flowbite/**/*.js",
     ],
+    safelist: [
+        // Colores de acento dinámicos para matrimonio (blue, pink, violet, teal)
+        // Tailwind v3: usar variants[] en lugar de prefijo en el pattern
+        {
+            pattern: /^bg-(blue|pink|violet|teal|emerald|amber|sky|rose|red)-(50|100|200|300|400|500|600|700|800|900)$/,
+            variants: ['hover', 'dark', 'dark:hover'],
+        },
+        {
+            pattern: /^border-(blue|pink|violet|teal|emerald|amber|sky|rose|red)-(50|100|200|300|400|500|600|700|800|900)$/,
+            variants: ['dark'],
+        },
+        {
+            pattern: /^text-(blue|pink|violet|teal|emerald|amber|sky|rose|red)-(50|100|200|300|400|500|600|700|800|900)$/,
+            variants: ['dark'],
+        },
+        {
+            pattern: /^ring-(blue|pink|violet|teal|emerald|amber|sky|rose|red)-(50|100|200|300|400|500|600|700|800|900)$/,
+            variants: ['focus'],
+        },
+        {
+            pattern: /^from-(blue|pink|violet|teal|emerald|amber|sky|rose|red)-(50|100|200|300|400|500|600|700|800|900)$/,
+            variants: ['dark'],
+        },
+    ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'purpura-sagrado': '#4A1A6B',
+                'dorado-divino':   '#C9A84C',
+                'azul-mariano':    '#1B3A6B',
+                'blanco-liturgico':'#F8F4EC',
+                'marfil-calido':   '#EDE8DC',
+                'rojo-martir':     '#8B1A1A',
+                'verde-esperanza': '#2D5A3D',
+                'gris-piedra':     '#6B6560',
+            },
+        },
     },
     plugins: [
         flowbite,
